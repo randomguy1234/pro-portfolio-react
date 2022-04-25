0786ef2faadee5e304627e7d64776cba6376e93e
Jimmy Rando <jsturat46925@gmail.com>
2022-04-24 23:48:23 -0400

    added links but need to add images and skill list

--- self-portfolio/src/components/Footer/footer.js
+++ self-portfolio/src/components/Footer/footer.js
@@ -3,7 +3,15 @@ import React from 'react';
 function Footer()
 {
     return (
-        <h1>Footer</h1>
+        <div className='flex flex-wrap justify-around'>
+            <a href='https://github.com/randomguy1234'>
+                <p>Github Link</p>
+            </a>
+
+            <a href='https://www.linkedin.com/in/michael-brown-39409817a/'>
+                <p>Linkedin Link</p>
+            </a>
+        </div>
     );
 }
 
--- self-portfolio/src/components/Navigation/resume.js
+++ self-portfolio/src/components/Navigation/resume.js
@@ -12,7 +12,17 @@ function Resume()
             <h2>Resume</h2>
             <p>Here is a list of some of the skills that I have.</p>
 
-            
+            {/** find a way to add this to navigation*/}
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>NoSQL</li>
+                <li>PWA</li>
+            </ul>
         </div>
     );
 }
